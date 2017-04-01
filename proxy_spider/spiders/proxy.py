c04dba4ad4384d4c859a6ace40cc4d5c3eb705c2
luocj <cnluocj@gmail.com>
2017-04-01 15:12:04 +0800

    User agent 版本不对，导致的 521 状态码错误

--- proxy_spider/spiders/proxy.py
+++ proxy_spider/spiders/proxy.py
@@ -15,19 +15,21 @@ class ProxySpider(Spider):
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Encoding': 'gzip, deflate, sdch',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
         'Accept-Language': 'en-US,en;q=0.8,zh-TW;q=0.6,zh;q=0.4',
-        'Connection': 'keep-alive',
+        # 'Connection': 'keep-alive',
         'Referer': 'http://www.kuaidaili.com/',
+        'Upgrade-Insecure-Requests': 1,
+        'Cache-Control': 'max-age=0',
     }
 
     cookies = {
-        '_ydclearance': '6ea840b202be71da37b2a26f-12f2-4236-8518-9cbefca79ba2-1490981935',
+        '_ydclearance': '05802e42ea361c37251d6c30-6ed2-4c1e-ae06-69243190dc41-1491035318',
         'channelid': '0',
-        'sid': '1490976247707954',
-        'Hm_lvt_7ed65b1cc4b810e9fd37959c9bb51b31': '1490974747',
-        'Hm_lpvt_7ed65b1cc4b810e9fd37959c9bb51b31': '1490976307',
-        '_ga': 'GA1.2.1602496365.1490974747',
+        'sid': '1491028837567397',
+        # 'Hm_lvt_7ed65b1cc4b810e9fd37959c9bb51b31': '1490974747',
+        # 'Hm_lpvt_7ed65b1cc4b810e9fd37959c9bb51b31': '1490976307',
+        # '_ga': 'GA1.2.1602496365.1490974747',
     }
 
     def start_requests(self):
@@ -40,13 +42,14 @@ class ProxySpider(Spider):
 
     def parse(self, response):
         for sel in response.xpath("//tbody/tr"):
-            proxy = Proxy()
-            proxy['ip'] = sel.xpath("td[@data-title='IP']/text()").extract()[0]
-            proxy['port'] = sel.xpath("td[@data-title='PORT']/text()").extract()[0]
-            proxy['faceless'] = sel.xpath("td[@data-title=%s]/text()" % u'匿名度').extract()[0]
-            proxy['type'] = sel.xpath("td[@data-title=%s]/text()" % u'类型').extract()[0]
-            proxy['method'] = sel.xpath("td[@data-title=%s]/text()" % u'get/post支持').extract()[0]
-            proxy['address'] = sel.xpath("td[@data-title=%s]/text()" % u'地址').extract()[0]
-            proxy['respSpeed'] = sel.xpath("td[@data-title=%s]/text()" % u'响应速度').extract()[0]
-            proxy['lastVerifyAt'] = sel.xpath("td[@data-title=%s]/text()" % u'最后验证时间').extract()[0]
-            yield proxy
+                proxy = Proxy()
+                proxy['ip'] = sel.xpath("td[@data-title='IP']/text()").extract()[0]
+                proxy['port'] = sel.xpath("td[@data-title='PORT']/text()").extract()[0]
+                proxy['faceless'] = sel.xpath("td[@data-title='%s']/text()" % u'匿名度').extract()[0]
+                proxy['type'] = sel.xpath("td[@data-title='%s']/text()" % u'类型').extract()[0]
+                proxy['method'] = sel.xpath("td[@data-title='%s']/text()" % u'get/post支持').extract()[0]
+                proxy['address'] = sel.xpath("td[@data-title='%s']/text()" % u'位置').extract()[0]
+                proxy['respSpeed'] = sel.xpath("td[@data-title='%s']/text()" % u'响应速度').extract()[0]
+                proxy['lastVerifyAt'] = sel.xpath("td[@data-title='%s']/text()" % u'最后验证时间').extract()[0]
+                yield proxy
+
